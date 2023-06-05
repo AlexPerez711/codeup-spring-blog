8477df63a6c7ca1095eafe73b6ef1ab0147ad489
Alex Perez <alex.f.perez92@gmail.com>
2023-06-05 11:07:32 -0500

    controllers exercise

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -0,0 +1,35 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts() {
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postsById(@PathVariable String id) {
+        return "<h1>View an " + id + " post </h1>";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String getCreate() {
+        return "<h1>View the form for creating a post</h1>";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String postCreate() {
+        return "<h1>Create a new post </h1>";
+    }
+}
