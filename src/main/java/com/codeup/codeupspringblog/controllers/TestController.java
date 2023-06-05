7ea503ab15d21ce576488d3cccbc3fa626199189
Alex Perez <alex.f.perez92@gmail.com>
2023-06-05 10:29:30 -0500

    controllers set up exercise

---
+++ src/main/java/com/codeup/codeupspringblog/controllers/TestController.java
@@ -0,0 +1,33 @@
+package com.codeup.codeupspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/parks/{park}/{message}")
+    @ResponseBody
+    public String parks(@PathVariable String park, @PathVariable String message) {
+        return "<h1>Welcome to " + park + "!</h1><p>" + message + "</p>";
+    }
+
+    @GetMapping("/parks")
+    @ResponseBody
+    public String parks() {
+        return "<h1>Welcome to Jurassic Park!</h1>";
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+
+}
