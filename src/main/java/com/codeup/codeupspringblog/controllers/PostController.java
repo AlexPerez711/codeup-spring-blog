031dca24661dcffc2cbb35366202d9bd289f7e90
Alex Perez <alex.f.perez92@gmail.com>
2023-06-09 09:28:35 -0500

    services

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,80 +1,82 @@
 package com.codeup.codeupspringblog.controllers;
 
 import com.codeup.codeupspringblog.models.Post;
-import com.codeup.codeupspringblog.models.PostCategories;
 import com.codeup.codeupspringblog.models.User;
 import com.codeup.codeupspringblog.repositories.PostCategoriesRepository;
 import com.codeup.codeupspringblog.repositories.PostRepository;
 import com.codeup.codeupspringblog.repositories.UserRepository;
+import com.codeup.codeupspringblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class PostController {
     private final PostRepository postsDao;
     private final UserRepository userDao;
     private final PostCategoriesRepository catDao;
+    private final EmailService emailService;
 
-    public PostController(PostRepository postsDao, UserRepository userDao, PostCategoriesRepository catDao) {
+    public PostController(PostRepository postsDao, UserRepository userDao, PostCategoriesRepository catDao, EmailService emailService) {
         this.postsDao = postsDao;
         this.userDao = userDao;
         this.catDao = catDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
+
     public String viewPosts(Model model) {
         model.addAttribute("posts", postsDao.findAll());
         return "posts/index";
     }
 
+
     @GetMapping("/posts/{id}")
     public String singlePost(@PathVariable long id, Model model) {
-        model.addAttribute("post", postsDao.findById(id).orElse(null));
+        model.addAttribute("post", postsDao.findById(id).get());
         return "posts/show";
     }
 
+
     @GetMapping("/posts/create")
     public String showPostForm(Model model) {
+        // show categories in form
         model.addAttribute("categories", catDao.findAll());
+        // pass a new Post object to the form
         model.addAttribute("post", new Post());
         return "/posts/create";
     }
 
-    @GetMapping("/posts/{id}/edit")
-    public String showEditPostForm(@PathVariable long id, Model model) {
-        Post existingPost = postsDao.findById(id).orElse(null);
-//        if (existingPost == null) {
-//            return "error";
-//        }
-        model.addAttribute("post", existingPost);
-        return "/posts/edit";
-    }
-
-    @PostMapping("/posts/{id}/edit")
-    public String updatePost(@PathVariable long id, @ModelAttribute Post updatedPost) {
-        Post post = postsDao.findById(id).orElse(null);
-        if (post != null) {
-            post.setTitle(updatedPost.getTitle());
-            post.setBody(updatedPost.getBody());
-            postsDao.save(post);
-        }
-        return "redirect:/posts";
-    }
-
-
 
     @PostMapping("/posts/create")
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
     public String submitNewPost(@ModelAttribute Post post) {
-        User user = userDao.findById(1L).orElse(null);
-        if (user == null) {
-            return "error";
-        }
+        User user = userDao.findById(1L).get();
         post.setUser(user);
+
+        emailService.prepareAndSend(post, "New Post Created!", post.getBody());
         postsDao.save(post);
         return "redirect:/posts";
     }
+
+    @GetMapping("/posts/{id}/edit")
+    public String showEditForm(@PathVariable long id, Model model) {
+        if(postsDao.findById(id).isPresent()) {
+            Post postToEdit = postsDao.findById(id).get();
+            model.addAttribute("post", postToEdit);
+        }
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String updatePost(@ModelAttribute Post newPost) {
+        User user = userDao.findById(1L).get();
+        newPost.setUser(user);
+        postsDao.save(newPost);
+        return "redirect:/posts";
+    }
+
+
+
 }
--- src/main/java/com/codeup/codeupspringblog/controllers/TestController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/TestController.java
@@ -3,6 +3,7 @@ package com.codeup.codeupspringblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+
 @Controller
 public class TestController {
 
